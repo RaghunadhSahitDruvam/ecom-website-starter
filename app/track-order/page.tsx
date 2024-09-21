db97d5ed3a2ee575d79ac115900668a2e724857c
Raghunadh9 <konidoniravirr@gmail.com>
2024-09-21 13:21:55 +0530

    ok

--- app/track-order/page.tsx
+++ app/track-order/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Loader, Loader2, SearchIcon } from "lucide-react";
+import { Loader, SearchIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
